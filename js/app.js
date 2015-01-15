aa3dfac3d6280e0cc820e33051ed86379fff13fc
rm913 <rachelmakovoz@gmail.com>
2015-01-15 11:18:44 -0500

    k

--- js/app.js
+++ js/app.js
@@ -7,15 +7,15 @@ $(document).ready(function(){
 	var num = $('#number').val();
 
 
-	if (num % 3 == 0) {
+	if (num % 5 == 0 && num % 3 ==0) {
 		
-		$('.answer ul').append('<li>'+ "Fizz" + '</li>');
+		$('.answer ul').append('<li>'+ "Fizz-Buzz" + '</li>');
 	}
 	else if (num % 5 == 0) {
 		$('.answer ul').append('<li>'+ "Buzz" + '</li>');
 	}
-	else if (num % 5 == 0 && num % 3 ==0) {
-		$('.answer ul').append('<li>'+ "Fizz-Buzz" + '</li>');
+	else if ( num % 3 ==0) {
+		$('.answer ul').append('<li>'+ "Fizz" + '</li>');
 	}
 	else {
 		$('.answer ul').append('<li>'+ num + '</li>');
